dd6a60459f58fcb62f8ff6e7f7a83b1bb41a596c
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-06-14 11:12:24 +0700

    process bar token logic

--- src/components/Token/index.jsx
+++ src/components/Token/index.jsx
@@ -9,6 +9,20 @@ export default function Token({data,index}) {
 
     const DDAY = new Date(data.Data.lockTimestamp*1000+data.Data.lockHours*60*60*1000);
 
+    function secondsToDhms(seconds) {
+            seconds = Number(seconds);
+            var d = Math.floor(seconds / (3600*24));
+            var h = Math.floor(seconds % (3600*24) / 3600);
+            var m = Math.floor(seconds % 3600 / 60);
+            var s = Math.floor(seconds % 60);
+
+            var dDisplay = d > 0 ? d + (d === 1 ? " day, " : " days, ") : "0 days";
+            var hDisplay = h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : "0 hours";
+            var mDisplay = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "0 minutes";
+            var sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "0 seconds";
+            return dDisplay + hDisplay + mDisplay + sDisplay;
+        }
+
     const getTimeLeft = () => {
         var result = ( DDAY.getTime() - new Date().getTime() ) / 1000;
 
@@ -25,7 +39,6 @@ export default function Token({data,index}) {
             var sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "0 seconds";
             return dDisplay + hDisplay + mDisplay + sDisplay;
         }
-
         setTimeLeft(secondsToDhms(result))
     }
 
@@ -33,7 +46,11 @@ export default function Token({data,index}) {
         getTimeLeft()
     },1000)
 
-    let percentLeft = ((( DDAY.getTime() - new Date().getTime() ) / 1000/(DDAY.getTime()/1000))*1000).toFixed(0)
+    let totalDate = ( DDAY.getTime()/1000 - data.Data.lockTimestamp )
+    let dateLeft = (( DDAY.getTime() - new Date().getTime() ) / 1000)
+
+    const percentLeft = ((dateLeft/totalDate)*100)
+    
 
     const linkView = `https://bscscan.com/address/${vestingContract[index].vestingContract}`
 
